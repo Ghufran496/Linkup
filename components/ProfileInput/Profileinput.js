998e71750018279e393d7e87b7c8c9303b7e9375
Saad <saadullahbutt124@gmail.com>
2024-11-04 01:58:24 +0500

    login & input screen

--- components/ProfileInput/Profileinput.js
+++ components/ProfileInput/Profileinput.js
@@ -1,8 +1,92 @@
+// components/ProfileInputComponent.js
 import React from "react";
 import classes from "./ProfileInput.module.css";
 
 const ProfileInputComponent = () => {
-  return <div>ProfileInput component here</div>;
+  return (
+    <div className={classes.profileContainer}>
+      <div className={classes.row}>
+        <div className={classes.inputGroup}>
+          <label className={classes.label}>Whats your full name?</label>
+          <p className={classes.description}>Please fill in your name.</p>
+          <input
+            type="text"
+            className={classes.inputField}
+            placeholder="Name"
+          />
+        </div>
+
+        <div className={classes.inputGroup}>
+          <label className={classes.label}>
+            Would you like to display your full name?
+          </label>
+          <p className={classes.description}>
+            Do you want us to display your first and last name? Please choose a
+            format.
+          </p>
+          <div className={classes.displayOptions}>
+            <button className={classes.optionButton}>Max</button>
+            <button className={classes.optionButton}>Max M.</button>
+            <button className={classes.optionButton}>Max Muster</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.row}>
+        <div className={classes.inputGroup}>
+          <label className={classes.label}>How old are you?</label>
+          <p className={classes.description}>Please fill in your age.</p>
+          <input
+            type="number"
+            className={classes.inputField}
+            placeholder="Age"
+          />
+        </div>
+
+        <div
+          className={`${classes.inputGroup} ${classes.addPadding} ${classes.paddingforspace}`}
+        >
+          <label className={classes.label}>What is your gender?</label>
+          <p className={classes.description}>
+            Please tell us with what gender you identify with.
+          </p>
+          <input
+            type="text"
+            className={classes.inputField}
+            placeholder="Gender"
+          />
+        </div>
+      </div>
+
+      <div className={`${classes.inputGroup} ${classes.optionSpaceBottom}`}>
+        <label className={classes.label}>Where do you live?</label>
+        <p className={classes.description}>
+          Please tell us where you live or if you're traveling you can change
+          the location later on your profile page.
+        </p>
+        <input
+          type="text"
+          className={`${classes.inputField} ${classes.addPaddingRight}`}
+          placeholder="Location"
+        />
+      </div>
+
+      <div className={classes.row}>
+        <div className={classes.inputGroup}>
+          <label className={classes.label}>
+            Tell us about yourself (optional)
+          </label>
+          <p className={classes.description}>
+            If you tell us about yourself we can display more information to a
+            potential acquaintance/friend.
+          </p>
+          <textarea className={classes.textArea}></textarea>
+        </div>
+
+        <button className={classes.submitButton}>Submit</button>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileInputComponent;
--- components/RegistrationPage/Login.js
+++ components/RegistrationPage/Login.js
@@ -1,4 +1,29 @@
 import React from "react";
+import styles from "./Login.module.css";
 export default function Login() {
-  return <div style={{ backgroundColor: "red" }}>Login compoennt</div>;
+  return (
+    <div className={styles.loginContainer}>
+      <div className={styles.logo}>
+        <img src="/Images/logo.png" alt="Logo" />
+      </div>
+      <div className={styles.formContainer}>
+        <h2 className={styles.heading}>Email-Address</h2>
+        <input
+          type="email"
+          className={styles.inputField}
+          placeholder="Enter your email"
+        />
+        <h2 className={styles.heading}>Password</h2>
+        <input
+          type="password"
+          className={styles.inputField}
+          placeholder="Enter your password"
+        />
+        <div className={styles.buttonContainer}>
+          <button className={styles.loginButton}>Login</button>
+          <button className={styles.registerButton}>Register</button>
+        </div>
+      </div>
+    </div>
+  );
 }
